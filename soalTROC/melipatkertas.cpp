12face8b30b38ea6e0814aa34ea0c706b739fbf8
Viriya6 <viriyanand4@gmail.com>
2025-05-24 22:24:14 +0700

    latihan pt2

---
+++ soalTROC/melipatkertas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float l, w, a;
+	cin>>l>>w>>a;
+	float luas = l * w;
+	while(luas >= a){
+		if(luas == a){
+			cout<<"YA";
+			return 0;
+		} else {
+			luas /= 2;
+		}
+	}
+	cout<<"TIDAK";
+}
