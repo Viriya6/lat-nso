924fba1160f50e194d4beeaa3044cff43fb80333
Viriya6 <viriyanand4@gmail.com>
2025-05-24 21:44:53 +0700

    update

--- soalOSA2025/mengurutkanbebek.cpp
+++ soalOSA2025/mengurutkanbebek.cpp
@@ -3,18 +3,26 @@ using namespace std;
 
 int main() {
         int A, B, C;
-        int change;
+        int change = 0;
         cin>>A>>B>>C;
 
         while(A > B || B > C){
-                if(A > B){ swap(A, B); change++; }
-                if(B > C){ swap(B, C); change++; }
+                if(A > B){
+			swap(A, B); 
+			change++;
+		}
+                if(B > C){ 
+			swap(B, C); 
+			change++; 
+		}
 
         }
 
         if(A < B && B < C){
                 cout<<change;
-        } else{
+        } else {
                 cout<<"-1";
-        }
+	}
+
+	return 0;
 }
